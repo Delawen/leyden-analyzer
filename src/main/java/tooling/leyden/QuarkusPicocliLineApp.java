f49edffbdfe8d99eaceaa7d66a4d375e6586786e
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-04-20 12:14:34 +0200

    fix: Adding try-with-resources

--- src/main/java/tooling/leyden/QuarkusPicocliLineApp.java
+++ src/main/java/tooling/leyden/QuarkusPicocliLineApp.java
@@ -106,8 +106,9 @@ public class QuarkusPicocliLineApp implements Runnable, QuarkusApplication {
 
                 status = Status.getStatus(terminal);
                 status.setBorder(true);
-                Executors.newSingleThreadScheduledExecutor()
-                        .scheduleWithFixedDelay(QuarkusPicocliLineApp::updateStatus, 0, 1, SECONDS);
+                try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+                    executor.scheduleWithFixedDelay(QuarkusPicocliLineApp::updateStatus, 0, 1, SECONDS);
+                }
 
                 final var historyFileName = ".leyden-analyzer.history";
                 LineReader reader = LineReaderBuilder.builder()
