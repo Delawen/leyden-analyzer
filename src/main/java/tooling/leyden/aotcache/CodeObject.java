4c2ba2b0c73c00432b44f8bbe290ab72826f276a
María Arias de Reyna Domínguez <mariasde@ibm.com>
2026-06-10 14:09:15 +0200

    feat: adding support for https://github.com/openjdk/jdk/pull/30778 8380476: Implement JEP: Ahead-of-Time Code Compilation

---
+++ src/main/java/tooling/leyden/aotcache/CodeObject.java
@@ -0,0 +1,42 @@
+package tooling.leyden.aotcache;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
+
+/**
+ * This class represents an element on the Code Cache
+ */
+public class CodeObject extends ReferencingElement {
+
+    private Integer id = null;
+
+    CodeObject(String identifier, String type) {
+        super(identifier, type);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getKey() {
+        return "[" + this.getId() + "] " + super.getKey();
+    }
+
+    @Override
+    public AttributedString getDescription(String leftPadding, Boolean verbose, Boolean tips) {
+        AttributedStringBuilder sb = new AttributedStringBuilder();
+        sb.append(super.getDescription(leftPadding, verbose, tips));
+        if (verbose) {
+            sb.append(AttributedString.NEWLINE);
+            sb.append(leftPadding).append("This is part of the Code Cache.");
+        }
+        return sb.toAttributedString();
+    }
+
+}
